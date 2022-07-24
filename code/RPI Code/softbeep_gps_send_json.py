3c45429fed2bce50f8a2e25262418fd932fe1151
jamangstangs <jamangsong@gmail.com>
2022-07-24 16:41:03 +0900

    RPI코드

---
+++ code/RPI Code/softbeep_gps_send_json.py
@@ -0,0 +1,18 @@
+import socket
+# 통신 정보 설정
+IP = '172.17.2.139'
+PORT = 5050
+SIZE = 1024
+ADDR = (IP, PORT)
+
+# 서버 소켓 설정
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.bind(ADDR)
+    server_socket.listen()
+
+    # 무한루프 진입
+    while True:
+        client_socket, client_addr = server_socket.accept()
+        msg = client_socket.recv(SIZE)
+      
+        print(msg)
